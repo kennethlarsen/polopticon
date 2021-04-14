1624efebb49e23972d57d416f5568f79c2525c9c
kennethlarsen <mail@kennethlarsen.org>
2021-04-14 11:38:14 +0000

    Add: Naser Khader

---
+++ twitter/pol.py
@@ -0,0 +1,28 @@
+import os
+
+POLITICIANS = [
+    "naserkhaderdk",
+    "vestager",
+    "larsloekke",
+    "PSkipperEL",
+    "oestergaard",
+    "IdaAuken",
+    "Kristian_Jensen",
+    "SorenPape",
+    "Heunicke",
+    "uffeelbaek",
+    "PiaOlsen",
+    "Statsmin",
+    "regeringDK",
+    "sofiecn",
+    "JakobEllemann",
+    "ammitzbollbille",
+    "DanJoergensen",
+    "DanishMFA",
+    "mattiastesfaye"
+    
+]
+
+for politician in POLITICIANS:
+    print(politician)
+    #os.mkdir(politician)
